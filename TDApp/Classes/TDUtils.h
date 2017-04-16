a26b53a5cc10d879d9638057d175ca9088b3fbea
Tun <tun@Tuns-MacBook-Pro.local>
2017-04-16 14:47:09 +0700

    add utils

--- TDApp/Classes/TDUtils.h
+++ TDApp/Classes/TDUtils.h
@@ -7,11 +7,19 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <MBProgressHUD/MBProgressHUD.h>
 
 #define APP_NAME [TDUtils appname]
+
+#define SHOW_HUD(view,ani,message) [TDUtils showHUDAddedTo:view animated:ani msg:message]
+#define HIDE_HUD(view,ani) [TDUtils hideHUDForView:view animated:ani]
+
+#define JOIN(str1, str2) [TDUtils joinString:str1 withString:str2]
 
 @interface TDUtils : NSObject
 
 + (NSString *)appname;
++ (MBProgressHUD *)showHUDAddedTo:(UIView *)view animated:(BOOL)animated msg:(NSString *)msg;
++ (NSString *)joinString:(NSString *)str1 withString:(NSString *)str2;
 
 @end
--- TDApp/Classes/TDUtils.m
+++ TDApp/Classes/TDUtils.m
@@ -15,4 +15,17 @@
     return [[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"];
 }
 
++ (MBProgressHUD *)showHUDAddedTo:(UIView *)view animated:(BOOL)animated msg:(NSString *)msg
+{
+    MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:view animated:YES];
+    hud.mode = MBProgressHUDModeIndeterminate;
+    hud.label.text = msg;
+    return hud;
+}
+
++ (NSString *)joinString:(NSString *)str1 withString:(NSString *)str2
+{
+    return [NSString stringWithFormat:@"%@%@",str1, str2];
+}
+
 @end
